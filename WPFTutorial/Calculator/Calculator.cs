404df47402240a46afa2357bf2071186d7fd4f21
rreneker <rreneker@umich.edu>
2018-04-23 21:36:38 -0400

    First Commit

---
+++ WPFTutorial/Calculator/Calculator.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    public class Calculator
+    {
+        private string storedNumber;
+        private string currentNumber;
+        private string operation;
+        private bool overwrite;
+
+        public Calculator()
+        {
+            storedNumber = "";
+            currentNumber = "";
+            operation = "";
+            overwrite = false;
+        }
+
+        public void NumberInput(string number)
+        {
+            if(overwrite == true)
+            {
+                currentNumber = number;
+                overwrite = false;
+            }
+            else
+            {
+                currentNumber += number;
+            }
+            
+        }
+
+        public void Calculate(string operation)
+        {
+            if(operation == "Enter")
+            {
+                operation = "";
+            }
+            if(storedNumber == "")
+            {
+                storedNumber = currentNumber;
+                //currentNumber = "";
+                this.operation = operation;
+                overwrite = true;
+            }
+            else
+            {
+                if(this.operation == "+")
+                {
+                    double result = Convert.ToDouble(storedNumber) + Convert.ToDouble(currentNumber);
+                    currentNumber = result.ToString();
+                    storedNumber = "";
+                    this.operation = operation;
+                    overwrite = true;
+                }
+                else if(this.operation == "-")
+                {
+                    double result = Convert.ToDouble(storedNumber) - Convert.ToDouble(currentNumber);
+                    currentNumber = result.ToString();
+                    storedNumber = "";
+                    this.operation = operation;
+                    overwrite = true;
+                }
+                else if(this.operation == "*")
+                {
+                    double result = Convert.ToDouble(storedNumber) * Convert.ToDouble(currentNumber);
+                    currentNumber = result.ToString();
+                    storedNumber = "";
+                    this.operation = operation;
+                    overwrite = true;
+                }
+                else if(this.operation == "/")
+                {
+                    double result = Convert.ToDouble(storedNumber) / Convert.ToDouble(currentNumber);
+                    currentNumber = result.ToString();
+                    storedNumber = "";
+                    this.operation = operation;
+                    overwrite = true;
+                }
+            }
+        }
+        public void Clear()
+        {
+            operation = "";
+            currentNumber = "";
+            storedNumber = "";
+        }
+
+        public string GetCurrentNumber()
+        {
+            if(currentNumber == "")
+            {
+                return "0";
+            }
+            else
+            {
+                return currentNumber;
+            }
+            
+        }
+    }
+}
---
+++ WPFTutorial/WPFTutorial/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BusinessLogic;
+
+namespace WPFTutorial
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Calculator cal;
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+            
+            cal = new Calculator();
+            Result.Text = cal.GetCurrentNumber();
+            //MainGrid.MouseUp += new MouseButtonEventHandler(MainGrid_MouseUp);
+        }
+
+        private void One_Click(object sender, RoutedEventArgs e)
+        {
+            string button = (e.Source as Button).Content.ToString();
+            cal.NumberInput(button);
+            Result.Text = cal.GetCurrentNumber();
+            
+        }
+
+        private void Minus_Click(object sender, RoutedEventArgs e)
+        {
+            string button = (e.Source as Button).Content.ToString();
+            cal.Calculate(button);
+            Result.Text = cal.GetCurrentNumber();    
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            cal.Clear();
+            Result.Text = cal.GetCurrentNumber();
+        }
+    }
+}
